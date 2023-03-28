afa673ac258b4be020732e73141fbd5dae8cf5ec
Eric Lusk-Jopson <eluskjopson@gmail.com>
2023-03-27 20:01:54 -0700

    recorded first thoughts for approach

--- Day11/final.js
+++ Day11/final.js
@@ -2,4 +2,12 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {};
+var maxArea = function (height) {
+  // thoughts:
+  // find the largest two values in the array L1 and L2 (assume L1 is left of L2)
+  // calculate their area
+  // for (i = min(L1, L2); i > 0; i--)
+  // check if there is a value left of L1 equal to or greater than i
+  // check if there is a value right of L2 equal to or greater than i
+  // check the area of this rectangle, and if greater, record it as the output
+};
