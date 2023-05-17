64f928ae810aaf87ddcb123c7d8b86dadd68cc16
Eric Lusk-Jopson <eluskjopson@gmail.com>
2023-05-17 11:02:38 -0700

    completed day 14

--- Day14/final.js
+++ Day14/final.js
@@ -2,4 +2,21 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function (strs) {};
+var longestCommonPrefix = function (strs) {
+  //if I sort the array, then I only need to check the similarity of the first and last elements.
+  strs.sort();
+
+  const first = strs.at(0);
+  const last = strs.at(-1);
+
+  let i = 0;
+  let output = "";
+
+  while (i < first.length && i < last.length) {
+    if (first[i] === last[i]) {
+      output += first[i];
+      i++;
+    } else break;
+  }
+  return output;
+};
